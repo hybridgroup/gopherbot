a1f7a87f319aa73d7973d1b4c372009e6ae8310a
Ron Evans <ron@hybridgroup.com>
2019-07-06 15:10:53 +0200

    docs: add some useful GoDocs comments to the main package file.
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- gopherbot.go
+++ gopherbot.go
@@ -1,10 +1,15 @@
+// Package gopherbot is the TinyGo code for the Gopherbot programmable robotic plushie.
 package gopherbot
 
 import "github.com/tinygo-org/tinygo/src/machine"
 
 const (
+	// BackpackLEDCount is how many WS2812 LEDs are on the backpack, aka
+	// the Circuit Playground Express.
 	BackpackLEDCount = 10
-	VisorLEDCount    = 12
+
+	// VisorLEDCount is how many WS2812 LEDs are on the helmet visor.
+	VisorLEDCount = 12
 )
 
 var (
@@ -12,7 +17,8 @@ var (
 	i2cInitComplete = false
 )
 
-// EnsureADCInit makes sure that the Gopherbot ADC has been initialized.
+// EnsureADCInit makes sure that the Gopherbot ADC has been initialized, but
+// is only initialized once.
 func EnsureADCInit() {
 	if !adcInitComplete {
 		machine.InitADC()
@@ -20,7 +26,8 @@ func EnsureADCInit() {
 	}
 }
 
-// EnsureI2CInit makes sure that the Gopherbot I2C has been initialized.
+// EnsureI2CInit makes sure that the Gopherbot I2C has been initialized, but
+// is only initialized once.
 func EnsureI2CInit() {
 	if !i2cInitComplete {
 		machine.I2C1.Configure(machine.I2CConfig{})
@@ -34,6 +41,8 @@ func EnsureI2CInit() {
 //
 //		val := gopherbot.Rescale(25, 0, 100, 0, 10)
 //
+// This re-scales the number 25 from a scale that ranges from 0-100,
+// to a scale that ranges from 0-10.
 func Rescale(input, fromMin, fromMax, toMin, toMax int32) int32 {
 	return (input-fromMin)*(toMax-toMin)/(fromMax-fromMin) + toMin
 }
