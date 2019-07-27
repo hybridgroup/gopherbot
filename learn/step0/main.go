8ce4f8c97fc1a256bc47fda7c8a17a0366a06245
Ron Evans <ron@hybridgroup.com>
2019-07-27 06:05:27 -0700

    learn: adding step 0
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

---
+++ learn/step0/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	for {
+		led.High()
+		time.Sleep(time.Millisecond * 500)
+
+		led.Low()
+		time.Sleep(time.Millisecond * 500)
+	}
+}
