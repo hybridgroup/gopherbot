fd654224d418f9d71f3aec3c975ff4b0eff0f283
Ron Evans <ron@hybridgroup.com>
2019-10-16 12:51:17 +0200

    visor: refactor to make API and implementation match the Backpack
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- visor.go
+++ visor.go
@@ -11,7 +11,7 @@ import (
 type VisorDevice struct {
 	ws2812.Device
 	LED     []color.RGBA
-	rg      bool
+	alt     bool
 	forward bool
 	pos     int
 }
@@ -38,37 +38,45 @@ func (v *VisorDevice) Off() {
 	v.Clear()
 }
 
-// Clear clears the visor.
-func (v *VisorDevice) Clear() {
+// SetColor sets the Visor LEDs to a single color.
+func (v *VisorDevice) SetColor(color color.RGBA) {
 	for i := range v.LED {
-		v.LED[i] = color.RGBA{R: 0x00, G: 0x00, B: 0x00}
+		v.LED[i] = color
 	}
 
 	v.Show()
+}
+
+// Clear clears the visor.
+func (v *VisorDevice) Clear() {
+	v.SetColor(color.RGBA{R: 0x00, G: 0x00, B: 0x00})
 }
 
 // Red turns all of the Visor LEDs red.
 func (v *VisorDevice) Red() {
-	for i := range v.LED {
-		v.LED[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00}
-	}
-
-	v.Show()
+	v.SetColor(color.RGBA{R: 0xff, G: 0x00, B: 0x00})
 }
 
 // Green turns all of the Visor LEDs green.
 func (v *VisorDevice) Green() {
-	for i := range v.LED {
-		v.LED[i] = color.RGBA{R: 0x00, G: 0xff, B: 0x00}
-	}
-
-	v.Show()
+	v.SetColor(color.RGBA{R: 0x00, G: 0xff, B: 0x00})
 }
 
 // Blue turns all of the Visor LEDs blue.
 func (v *VisorDevice) Blue() {
+	v.SetColor(color.RGBA{R: 0x00, G: 0x00, B: 0xff})
+}
+
+// Alternate the 2 colors.
+func (v *VisorDevice) Alternate(color1, color2 color.RGBA) {
+	v.alt = !v.alt
 	for i := range v.LED {
-		v.LED[i] = color.RGBA{R: 0x00, G: 0x00, B: 0xff}
+		v.alt = !v.alt
+		if v.alt {
+			v.LED[i] = color1
+		} else {
+			v.LED[i] = color2
+		}
 	}
 
 	v.Show()
@@ -76,17 +84,7 @@ func (v *VisorDevice) Blue() {
 
 // Xmas light style
 func (v *VisorDevice) Xmas() {
-	v.rg = !v.rg
-	for i := range v.LED {
-		v.rg = !v.rg
-		if v.rg {
-			v.LED[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00}
-		} else {
-			v.LED[i] = color.RGBA{R: 0x00, G: 0xff, B: 0x00}
-		}
-	}
-
-	v.Show()
+	v.Alternate(color.RGBA{R: 0xff, G: 0x00, B: 0x00}, color.RGBA{R: 0x00, G: 0xff, B: 0x00})
 }
 
 // Cylon visor mode.
